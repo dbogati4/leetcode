d230cc862321afcac653436f6ef2eae35f96983f
dbogati4 <bogatidipika38@gmail.com>
2025-07-23 11:31:00 -0400

    IN self join think about >= as well

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetcode
+
+For sql: Youtube channel: Learn with chirag: https://www.youtube.com/@learnwithchirag 
---
+++ SQLQuestions/1204_Last_Person_to_Fit_In_The_Bus.sql
@@ -0,0 +1,8 @@
+SELECT Q1.person_name
+FROM Queue Q1 
+JOIN Queue Q2
+ON Q1.turn >= Q2.turn
+GROUP BY Q1.turn
+HAVING SUM(Q2.weight) <= 1000
+ORDER BY Q1.turn DESC
+LIMIT 1;
